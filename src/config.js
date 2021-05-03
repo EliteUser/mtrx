5e1cf28fbbeb479af8d2fa9efa7de9b126b525d9
Andrey <unknownuser017@gmail.com>
2021-05-03 22:31:14 +0300

    Add app config

---
+++ src/config.js
@@ -0,0 +1,31 @@
+export const AppScreen = {
+  ENTRY: 'entry',
+  EDITOR: 'editor'
+};
+
+export const filterConfig = {
+  'contrast': {
+    defaultValue: 1,
+    min: 0,
+    max: 2,
+    step: 0.05,
+    twoSided: true
+  },
+  'opacity': {
+    defaultValue: 1,
+    min: 0,
+    max: 1,
+    step: 0.01,
+    twoSided: false
+  },
+  'brightness': {
+    defaultValue: 1,
+    min: 0,
+    max: 2,
+    step: 0.01,
+    twoSided: true
+  }
+};
+
+export const defaultFilterValues = Object.entries(filterConfig)
+  .reduce((acc, el) => Object.assign(acc, {[el[0]]: el[1].defaultValue}), {});
