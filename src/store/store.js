e05ad67168ae8acf01b4c8d0cb0d04f4864877c8
Andrey <unknownuser017@gmail.com>
2021-05-12 16:24:35 +0300

    Add action log for debug (Redux devtools doesnt work)

--- src/store/store.js
+++ src/store/store.js
@@ -3,10 +3,18 @@ import {composeWithDevTools} from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/root-reducer';
 
+const logMiddleware = ({ getState }) => (next) => (action) => {
+  console.log(action.type, getState());
+  return next(action);
+};
+
 const store = createStore(
   rootReducer,
   composeWithDevTools(
-    applyMiddleware(thunk)
+    applyMiddleware(
+      thunk,
+      logMiddleware
+    )
   )
 );
 
