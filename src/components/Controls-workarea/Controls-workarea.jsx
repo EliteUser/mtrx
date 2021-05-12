06d49f6c017cbeca258e4304ea42312b335303d1
Andrey <unknownuser017@gmail.com>
2021-05-12 12:27:20 +0300

    Restyle controls-workarea (scroll-slider)

--- src/components/Controls-workarea/Controls-workarea.jsx
+++ src/components/Controls-workarea/Controls-workarea.jsx
@@ -36,88 +36,103 @@ const ControlsWorkarea = (props) => {
 
   return (
     <div className={style['controls-workarea']}>
-      <div className={style['controls-workarea__buttons']}>
-        <Button
-          isTextHidden={true}
-          text={'Change Matrix Mode'}
-          className={btnStyle['btn--accent']}
-          onBtnClick={onModeChange}
-          title={isAdvancedMode ? 'Standard Mode' : 'Advanced Mode'}
-        >
-          {
-            isAdvancedMode ?
-              <StandardModeIcon/> :
-              <AdvancedModeIcon/>
-          }
-        </Button>
-      </div>
+      <div className={style['controls-workarea__snap-container']}>
 
-      <div className={style['controls-workarea__matrix-size']}>
-        <p className={style['controls-workarea__label']}>Matrix Size</p>
-        <div className={style['controls-workarea__wrapper']}>
+        <div className={style['controls-workarea__controls']}>
+
+          <div className={style['controls-workarea__matrix-size']}>
+            <p className={style['controls-workarea__label']}>Matrix Size</p>
+            <Input
+              styleName={'integer'}
+              id={'kernelX'}
+              inputMode={'number'}
+              step={1}
+              min={1}
+              max={10}
+              value={kernelX}
+              onChange={(evt) => onKernelXChange(evt.target.value, id)}
+            />
+            <span>X</span>
+            <Input
+              styleName={'integer'}
+              id={'kernelY'}
+              inputMode={'number'}
+              step={1}
+              min={1}
+              max={10}
+              value={kernelY}
+              onChange={(evt) => onKernelYChange(evt.target.value, id)}
+            />
+          </div>
           <Input
-            styleName={'integer'}
-            id={'kernelX'}
-            inputMode={'number'}
-            step={1}
-            min={1}
-            max={10}
-            value={kernelX}
-            onChange={(evt) => onKernelXChange(evt.target.value, id)}
+            className={style['controls-workarea__input--divisor']}
+            label={'Divisor'}
+            step={0.1}
+            value={divisor}
+            onChange={(evt) => onDivisorChange(evt.target.value, id)}
           />
           <Input
-            styleName={'integer'}
-            id={'kernelY'}
-            inputMode={'number'}
-            step={1}
-            min={1}
-            max={10}
-            value={kernelY}
-            onChange={(evt) => onKernelYChange(evt.target.value, id)}
+            className={style['controls-workarea__input--bias']}
+            label={'Bias'}
+            step={0.1}
+            value={bias}
+            onChange={(evt) => onBiasChange(evt.target.value, id)}
           />
+
         </div>
-      </div>
-      <Input
-        className={style['controls-workarea__input--divisor']}
-        label={'Divisor'}
-        step={0.1}
-        value={divisor}
-        onChange={(evt) => onDivisorChange(evt.target.value, id)}
-      />
-      <Input
-        className={style['controls-workarea__input--bias']}
-        label={'Bias'}
-        step={0.1}
-        value={bias}
-        onChange={(evt) => onBiasChange(evt.target.value, id)}
-      />
 
-      <div className={style['controls-workarea__matrix']}>
-        <p className={style['controls-workarea__title']}>Matrix</p>
+      </div>
+
+      <div className={style['controls-workarea__snap-container']}>
         {
           isAdvancedMode ?
-            <MatrixTextareaContainer
-              value={kernelMatrix}
-              kernelX={kernelX}
-              primitiveId={id}
-              name={'matrix'}
-              onChange={(evt) => onKernelMatrixChange(evt.target.value, id)}
-            /> :
-            <MatrixInputsContainer
-              id={id}
-              kernelX={kernelX}
-              kernelY={kernelY}
-              kernelMatrix={kernelMatrix}
-            />
+            <div className={`${style['controls-workarea__matrix-textarea']}`}>
+              <MatrixTextareaContainer
+                value={kernelMatrix}
+                kernelX={kernelX}
+                primitiveId={id}
+                name={'matrix'}
+                onChange={(evt) => onKernelMatrixChange(evt.target.value, id)}
+              />
+            </div> :
+
+            <div className={`${style['controls-workarea__matrix-inputs']}`}>
+              <MatrixInputsContainer
+                id={id}
+                kernelX={kernelX}
+                kernelY={kernelY}
+                kernelMatrix={kernelMatrix}
+              />
+            </div>
         }
+
+        <div className={style['controls-workarea__buttons']}>
+          <Button
+            isTextHidden={true}
+            text={'Change Matrix Mode'}
+            className={btnStyle['btn--accent']}
+            onBtnClick={onModeChange}
+            title={isAdvancedMode ? 'Standard Mode' : 'Advanced Mode'}
+          >
+            {
+              isAdvancedMode ?
+                <StandardModeIcon/> :
+                <AdvancedModeIcon/>
+            }
+          </Button>
+
+
+        </div>
+
       </div>
+
     </div>
   );
 };
 
 ControlsWorkarea.propTypes = {
 
-  //TODO REFACTOR PROPS DUPLICATE TO ...REST
+//TODO REFACTOR PROPS DUPLICATE TO ...REST
 };
 
 export default ControlsWorkarea;
